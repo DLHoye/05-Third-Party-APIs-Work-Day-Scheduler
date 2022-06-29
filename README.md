743ef482a3b350dd42e20ff0bfb5c41504511bf9
Derek Hoye <dhoye144@gmail.com>
2022-06-28 23:53:09 -0400

    I need to commit more often. I keep doing this last second!

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# 05-Third-Party-APIs-Work-Day-Scheduler
-Derek's Calendar Application
+Hi there! 
+
+Follow the link below to open a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. 
+
+This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+This calendar uses the Moment.js library to work with date/time and make sure you're not late for that all important meeting/hunny-do list!
+
+Once you've entered an event into the hourly time blocks, click the save button so you don't lose your stuff! 
+
+At the end of the day the calendar will clear itself and you can start alllllll over again!
+
+https://dlhoye.github.io/05-Third-Party-APIs-Work-Day-Scheduler/
+
+<img src='assets\images\05-third-party-apis-homework-demo.gif'>
---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+//const list + moment for date
+const currentDayEl = $("#currentDay");
+const today = moment().format("LLLL");
+currentDayEl.text(today);
+
+//pull events from local storage
+let events = JSON.parse(localStorage.getItem("DailyPlanner"));
+if (!events) {
+  events = {};
+  events[today] = {};
+}
+
+
+//build each hour of the workday
+const createEachHour = () => {
+  const eachHour = $(".container");
+  for (i = 8; i <= 17; i++) {
+    const hour = moment(i, "H").format("hA");
+    const row = $(`<div id=${hour} class="row time-block">`);
+    const timeSlot = $('<textarea class="col-10 form-control">');
+
+    eachHour.append(row);
+    row.append(timeSlot);
+
+    timeSlot
+      .before(`<div class="hour col pt-4">${hour}</div>`)
+      .after('<button class="col btn saveBtn" id="save">💾</button>');
+    
+    //add events to each hour
+     if (events[today][hour]) $(`#${hour}`).children("textarea").val(events[today][hour]);
+  }
+  checkTime();
+  addListener();
+};
+
+//check hour to asign past, present, or future css styles
+const checkTime = () => {
+  $("textarea").each((i, el) => {
+    const prev = $(el).prev();
+    const now = moment();
+    const prevTime = moment(prev.text(), "hA");
+    if (prevTime.format('H') === now.format('H')) {
+      $(el).addClass("present");
+    }
+    else if (prevTime.isBefore(now)) {
+      $(el).addClass("past");
+    } else if (prevTime.isAfter(now)) {
+      $(el).addClass("future");
+    } 
+  });
+};
+
+//save events to local storage
+const saveEvent = (e) => {
+  const event = $(e.target).prev()
+  const eventTime = $(event).prev().text()
+  events[today][eventTime] = event.val()
+  localStorage.setItem('DailyPlanner', JSON.stringify(events))
+}
+
+//add save button click event listener
+const addListener = () => {
+   $("button").on("click", saveEvent);
+}
+
+//checks the time every 10 seconds for any CSS style updates
+setInterval(checkTime, 1000 * 10);
+
+//calls the createEachHour function
+$(window).on("load", createEachHour);
